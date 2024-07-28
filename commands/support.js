e029a89c319731c2b158b3829b26d479fa28a71a
famef15 <121099581+famef15@users.noreply.github.com>
2024-07-28 20:20:31 +0700

    support.js

--- commands/support.js
+++ commands/support.js
@@ -29,19 +29,17 @@ module.exports = {
   run: async (client, interaction) => {
     try {
 
-      const supportServerLink = "https://discord.gg/xQF9f9yUEM";
-      const githubLink = "https://github.com/GlaceYT";
-      const replitLink = "https://replit.com/@GlaceYT";
-      const youtubeLink = "https://www.youtube.com/@GlaceYT";
+      const supportServerLink = "https://discord.gg/uGk8QcZMRs";
+      const youtubeLink = "https://www.youtube.com/@zerox-sb3dr";
         const embed = new EmbedBuilder()
             .setColor('#b300ff')
             .setAuthor({
               name: 'Support Server',
-              iconURL: 'https://cdn.discordapp.com/attachments/1230824451990622299/1230824519220985896/6280-2.gif?ex=6638ae28&is=66375ca8&hm=13e4a1b91a95b2934a39de1876e66c11711c7b30ac1a91c2a158f2f2ed1c2fc6&', 
-              url: 'https://discord.gg/xQF9f9yUEM'
+              iconURL: 'https://cdn.discordapp.com/attachments/1122876346117398608/1267108098057703454/A07.png?ex=66a795e0&is=66a64460&hm=9504f7faab147efb82f09e845e48bfa72be8c46745509bf469d35c53409a1e8f&', 
+              url: 'https://discord.gg/uGk8QcZMRs'
           })
-            .setDescription(`➡️ **Join our Discord server for support and updates:**\n- Discord - ${supportServerLink}\n\n➡️ **Follow us on:**\n- GitHub - ${githubLink}\n- Replit - ${replitLink}\n- YouTube - ${youtubeLink}`)
-            .setImage('https://cdn.discordapp.com/attachments/1113800537402527903/1236803979996958740/11.png?ex=663956f7&is=66380577&hm=3b3c19a11adcb979517a133f2907f671305d23f1f5092cf7df043e6d5cab07bc&')
+            .setDescription(`➡️ **Join our Discord server for support and updates:**\n- Discord - ${supportServerLink}\n\n➡️ **Follow us on:**\n- YouTube - ${youtubeLink}`)
+            .setImage('https://cdn.discordapp.com/attachments/1122876346117398608/1261582677409271979/RainbowLine.gif?ex=66a7426d&is=66a5f0ed&hm=aef4c75a79f2c6788d1c11212e9d8ac10441da8dd09b00219e4d7da9ec82d16b&')
             .setTimestamp();
 
       return interaction.reply({ embeds: [embed] });
