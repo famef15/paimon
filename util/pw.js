fef125a6049822e7efeec16103317c32c45aa2b4
famef15 <121099581+famef15@users.noreply.github.com>
2024-07-29 20:24:35 +0700

    Update pw.js

--- util/pw.js
+++ util/pw.js
@@ -1,6 +1,6 @@
 const status = "AUTOMATIC";
-const botName = "💎 PRIME MUSIC";
-const Text = "GlaceYT";
+const botName = "💎 MUSIC";
+const Text = "cfame";
 const version = "Latest@ v1.0"; 
 const startTime = Date.now();
 
