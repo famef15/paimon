0e111508ba1baf43ed5668445cc9c806acc9fe8d
famef15 <121099581+famef15@users.noreply.github.com>
2024-07-29 15:11:38 +0700

    Update config.js

--- config.js
+++ config.js
@@ -41,9 +41,11 @@ module.exports = {
   nodes: [
     {
         name: "banana man",
-        host: "sg1.elysiumx.xyz",
-        port: 1845,
-        password: "MachasLavalink48sa68",
+        host: "lava-v3.ajieblogs.eu.org",
+        port: 80,
+        password: "https://dsc.gg/ajidevserver",
+        reconnectTimeout: 5000,
+        reconnectTries: Infinity,
         secure: false
     },
  ]
